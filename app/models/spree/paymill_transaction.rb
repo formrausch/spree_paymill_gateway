918c1f217318f05b4a15ef0ab19358c645e4c2cc
Mike Lieser <mikelieser@me.com>
2015-01-23 15:33:49 +0100

    Revert 3b75566..b384b18
    This rolls back to commit 3b755669ae83f33535e89f6e623c652b81aedaff.

--- app/models/spree/paymill_transaction.rb
+++ app/models/spree/paymill_transaction.rb
@@ -2,12 +2,11 @@ require "paymill"
 
 module Spree
   class PaymillTransaction < ActiveRecord::Base
-    belongs_to :user
-    belongs_to :payment_method
 
     has_many :payments, as: :source
 
     serialize :payment_response, Paymill::Payment
     serialize :transaction_response, Paymill::Transaction
+
   end
 end
--- db/migrate/20140804144238_add_payment_method_id_and_user_id_to_spree_paymill_transactions.rb
+++
@@ -1,6 +0,0 @@
-class AddPaymentMethodIdAndUserIdToSpreePaymillTransactions < ActiveRecord::Migration
-  def change
-    add_reference :spree_paymill_transactions, :payment_method, index: true
-    add_reference :spree_paymill_transactions, :user, index: true
-  end
-end
